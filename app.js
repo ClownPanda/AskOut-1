033c165d1564bf091b84012efe2e8de9fa8e23d7
ClownPanda <103530322+ClownPanda@users.noreply.github.com>
2024-02-03 23:03:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -42,6 +42,32 @@ btnNo.addEventListener("mouseover", (e)=> {
     h1.innerText="Please select yes";
 
 })
+btnNo.addEventListener("touchstart", (e)=> {
+    const containerHeight = container.getBoundingClientRect().height;
+    const containerWidth = container.getBoundingClientRect().width;
+    const btnHeight = btnNo.getBoundingClientRect().height;
+    const btnWidth = btnNo.getBoundingClientRect().width;
+    const btnTop = btnNo.getBoundingClientRect().top;
+    const btnLeft = btnNo.getBoundingClientRect().left;
+
+    let newTop=btnTop;
+    let newLeft=btnLeft;
+
+    while(Math.abs(newTop - btnTop) < containerHeight / 3){
+        newTop = getRandomNumber(0, containerHeight - btnHeight);
+    }
+   
+    while(Math.abs(newLeft - btnLeft) < containerWidth / 3){
+        newLeft = getRandomNumber(0, containerWidth - btnWidth);
+    }
+     
+    btnNo.style.top =  Math.floor(newTop) + "px";
+    btnNo.style.left =  Math.floor(newLeft) + "px";
+    no.classList.remove("hide");
+    erm.classList.add("hide");
+    h1.innerText="Please select yes";
+
+})
 
 
 btnYes.addEventListener("click", (e) =>{
